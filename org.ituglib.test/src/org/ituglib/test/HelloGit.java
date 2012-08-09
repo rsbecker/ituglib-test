7db0569b9232d8ef585897f635772be386e73cef
Randall S. Becker <rsbecker@xanth.nexbridgeykz.local>
2012-08-09 16:07:34 -0400

    Initial test version

---
+++ org.ituglib.test/src/org/ituglib/test/HelloGit.java
@@ -0,0 +1,25 @@
+/*
+ * Property of Nexbridge Inc.
+ * Copyright © 2012 All Rights Reserved.
+ *
+ * Proprietary and Confidential. Disclosure to anyone other than Nexbridge employees is
+ * prohibited and will be prosecuted to the fullest extent of the law.
+ *
+ */
+
+package org.ituglib.test;
+
+/**
+ * @author rsbecker
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+	}
+
+}
